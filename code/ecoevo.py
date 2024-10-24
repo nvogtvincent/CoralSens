59af475b3d2b7c7a1dc3181b646a48d51e9fb556
nvogtvincent <nvogtvincent@outlook.com>
2024-10-23 17:51:18 -1000

    Added initial conditions

--- code/ecoevo.py
+++ code/ecoevo.py
@@ -22,31 +22,31 @@ class simulation:
                        'initial': False,
                        'boundary': False}
     
-    def set_bc(self, arr): 
+    def set_bc(self, bc=None): 
         '''
         Set the boundary conditions for a simulation. 
-           arr: numpy array or xarray DataArray
+           bc: numpy array or xarray DataArray with values of T
                 Either [i * j] or [1 * j] or [j]
         ''' 
         
         # Check input has correct datatype 
-        if type(arr) not in [xr.DataArray, np.ndarray]:
+        if type(bc) not in [xr.DataArray, np.ndarray]:
             print('Hey')
             raise Exception('Boundary conditions must be a numpy array or xarray DataArray.')
-        elif type(arr) == xr.DataArray:
-            arr = arr.data
+        elif type(bc) == xr.DataArray:
+            bc = bc.data
         
         # Get dimensions of array, check if consistent with records
         _i, _j = None, None
-        if arr.ndim == 1:
-            _j = len(arr)
-        elif arr.ndim == 2:
-            if arr.shape[0] == 1:
-                _j = len(arr)
-                arr = arr.flatten()
+        if bc.ndim == 1:
+            _j = len(bc)
+        elif bc.ndim == 2:
+            if bc.shape[0] == 1:
+                _j = len(bc)
+                bc = bc.flatten()
             else:
-                _j = arr.shape[1]
-                _i = arr.shape[0]
+                _j = bc.shape[1]
+                _i = bc.shape[0]
         else:
             raise Exception('Boundary conditions must be 1D or 2D.')
         
@@ -62,7 +62,56 @@ class simulation:
             self.j = _j
         
         # Save boundary conditions
-        self.bc = arr
+        self.bc = bc
             
         # Update status
         self.status['boundary'] = True
+    
+    def set_ic(self, z=None, c=None):
+        '''
+        Set the initial conditions for a simulation. 
+            z: numpy array or xarray DataArray (thermal optimum)
+                Either [i] or [i * 1] or scalar
+            c: numpy array or xarray DataArray (coral cover)
+                Either [i] or [i * 1] or scalar
+        ''' 
+        
+        # Get dimensions of array, check if consistent with records
+        if z is not None:
+            if z.ndim == 1:
+                _i = len(z)
+            elif z.ndim == 2:
+                if z.shape[1] == 1:
+                    _i = len(z)
+                    z = z.flatten()
+                else:
+                    raise Exception('Initial conditions must be at most 1D.')
+            else:
+                raise Exception('Initial conditions must be at most 1D.')
+            
+            if hasattr(self, 'i') and _i != self.i:
+                raise Exception('Inconsistent number of sites. Reset the simulation.')
+            else:
+                self.i = _i
+            
+            self.z0 = z
+        
+        if c is not None:
+            if c.ndim == 1:
+                _i = len(c)
+            elif c.ndim == 2:
+                if c.shape[1] == 1:
+                    _i = len(c)
+                    c = c.flatten()
+                else:
+                    raise Exception('Initial conditions must be at most 1D.')
+            else:
+                raise Exception('Initial conditions must be at most 1D.')
+            
+            if hasattr(self, 'c') and _c != self.c:
+                raise Exception('Inconsistent number of sites. Reset the simulation.')
+            else:
+                self.c = _c
+            
+            self.c0 = c
+            
